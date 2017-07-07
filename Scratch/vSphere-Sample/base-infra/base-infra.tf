bd5bfe9a0528d75db92e7aa66761f158ba20addd
Kevin Kirkpatrick <kevin@vmotioned.com>
2017-07-07 16:59:51 -0400

    Cleanup
    Signed-off-by: Kevin Kirkpatrick <kevin@vmotioned.com>

--- Scratch/vSphere-Sample/base-infra/base-infra.tf
+++
@@ -1,7 +0,0 @@
-# create a folder
-resource "vsphere_folder" "terraform-test" {
-    name = "terraform-test"
-    datacenter = "DC1"
-    description = "Terraform Test Folder"
-    path = "terraform-test"
-}
